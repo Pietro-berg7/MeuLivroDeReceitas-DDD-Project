af5af113ad96d0c4b13a52cc46cc5684a9d9a100
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-10-04 19:04:05 -0300

    Registrar Usuário: Validator

---
+++ src/Backend/MeuLivroDeReceitas.Application/UseCases/Usuario/Registrar/RegistrarUsuarioUseCase.cs
@@ -0,0 +1,23 @@
+﻿using MeuLivroDeReceitas.Comunicacao.Requisicoes;
+
+namespace MeuLivroDeReceitas.Application.UseCases.Usuario.Registrar;
+
+public class RegistrarUsuarioUseCase
+{
+    public Task Executar(RequisicaoRegistrarUsuarioJson requisicao)
+    {
+
+    }
+
+    private void Validar(RequisicaoRegistrarUsuarioJson requisicao)
+    {
+        var validator = new RegistrarUsuarioValidator();
+        var resultado = validator.Validate(requisicao);
+
+        if (!resultado.IsValid)
+        {
+            var mensagensDeErro = resultado.Errors.Select(error => error.ErrorMessage);
+            throw new Exception();
+        }
+    }
+}
