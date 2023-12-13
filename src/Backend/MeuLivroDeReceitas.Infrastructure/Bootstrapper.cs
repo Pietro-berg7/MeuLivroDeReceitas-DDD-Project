26ab41a18103472f36c7da6a9a578e8c75883d45
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-12-13 18:46:10 -0300

    criando funções no repositório para alterar senha

--- src/Backend/MeuLivroDeReceitas.Infrastructure/Bootstrapper.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Bootstrapper.cs
@@ -1,6 +1,7 @@
 ﻿using FluentMigrator.Runner;
 using MeuLivroDeReceitas.Domain.Extension;
 using MeuLivroDeReceitas.Domain.Repositorios;
+using MeuLivroDeReceitas.Domain.Repositorios.Usuario;
 using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio;
 using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio.Repositorio;
 using Microsoft.EntityFrameworkCore;
@@ -45,7 +46,8 @@ public static class Bootstrapper
     {
         services
             .AddScoped<IUsuarioWriteOnlyRepositorio, UsuarioRepositorio>()
-            .AddScoped<IUsuarioReadOnlyRepositorio, UsuarioRepositorio>();
+            .AddScoped<IUsuarioReadOnlyRepositorio, UsuarioRepositorio>()
+            .AddScoped<IUpdateOnlyRepositorio, UsuarioRepositorio>();
     }
 
     public static void AddFluentMigrator(IServiceCollection services, IConfiguration configurationManager)
