3c8f6f9a8afd1b996d32bfdd49992175b221a1f3
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-12-15 04:25:37 -0300

    corrige alguns bugs

--- src/Backend/MeuLivroDeReceitas.Domain/Repositorios/Usuario/IUsuarioUpdateOnlyRepositorio.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MeuLivroDeReceitas.Domain.Repositorios.Usuario;
-
-public interface IUsuarioUpdateOnlyRepositorio
-{
-    void Update(Entidades.Usuario usuario);
-    Task<Entidades.Usuario> RecuperarPorId(long id);
-}
