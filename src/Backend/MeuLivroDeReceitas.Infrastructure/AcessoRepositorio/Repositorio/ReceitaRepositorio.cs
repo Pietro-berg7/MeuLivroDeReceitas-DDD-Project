aef81012db4af9d5211a16edb6eea95be684f274
Pietro Bergamaschi <pietrovb25@gmail.com>
2024-01-03 20:45:11 -0300

    feat: adiciona repositório para recuperar dashboard

--- src/Backend/MeuLivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/ReceitaRepositorio.cs
@@ -1,14 +1,20 @@
 ﻿using MeuLivroDeReceitas.Domain.Entidades;
 using MeuLivroDeReceitas.Domain.Repositorios.Receita;
+using Microsoft.EntityFrameworkCore;
 
 namespace MeuLivroDeReceitas.Infrastructure.AcessoRepositorio.Repositorio;
-public class ReceitaRepositorio: IReceitaWriteOnlyRepositorio
+public class ReceitaRepositorio: IReceitaWriteOnlyRepositorio, IReceitaReadOnlyRepositorio
 {
     private readonly MeuLivroDeReceitasContext _contexto;
 
     public ReceitaRepositorio(MeuLivroDeReceitasContext contexto)
     {
         _contexto = contexto;
+    }
+
+    public async Task<IList<Receita>> RecuperarTodasDoUsuario(long usuarioId)
+    {
+        return await _contexto.Receitas.Where(r => r.UsuarioId == usuarioId).ToListAsync();
     }
 
     public async Task Registrar(Receita receita)
