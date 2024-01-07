a611d24350a4709e9f7eeeac8ae8ca245f9069a6
Pietro Bergamaschi <pietrovb25@gmail.com>
2024-01-07 14:06:16 -0300

    feat: adiconando minutos para o preparo da receita

--- src/Backend/MeuLivroDeReceitas.Application/UseCases/Receita/Registrar/RegistrarReceitaUseCase.cs
+++ src/Backend/MeuLivroDeReceitas.Application/UseCases/Receita/Registrar/RegistrarReceitaUseCase.cs
@@ -9,10 +9,10 @@ using MeuLivroDeReceitas.Exceptions.ExceptionsBase;
 namespace MeuLivroDeReceitas.Application.UseCases.Receita.Registrar;
 public class RegistrarReceitaUseCase: IRegistrarReceitaUseCase
 {
-    private IMapper _mapper;
-    private IUnidadeDeTrabalho _unidadeDeTrabalho;
-    private IUsuarioLogado _usuarioLogado;
-    private IReceitaWriteOnlyRepositorio _repositorio;
+    private readonly IMapper _mapper;
+    private readonly IUnidadeDeTrabalho _unidadeDeTrabalho;
+    private readonly IUsuarioLogado _usuarioLogado;
+    private readonly IReceitaWriteOnlyRepositorio _repositorio;
 
     public RegistrarReceitaUseCase(IMapper mapper, IUnidadeDeTrabalho unidadeDeTrabalho, IUsuarioLogado usuarioLogado, IReceitaWriteOnlyRepositorio repositorio)
     {
@@ -38,7 +38,7 @@ public class RegistrarReceitaUseCase: IRegistrarReceitaUseCase
         return _mapper.Map<RespostaReceitaJson>(receita);
     }
 
-    private void Validar(RequisicaoReceitaJson requisicao)
+    private static void Validar(RequisicaoReceitaJson requisicao)
     {
         var validator = new RegistrarReceitaValidator();
         var resultado = validator.Validate(requisicao);
