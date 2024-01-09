0b9e7c25911d61b39bb89875044a4468cc6896e7
Pietro Bergamaschi <pietrovb25@gmail.com>
2024-01-09 18:13:59 -0300

    refactor: alterar regra negocio recuperar todas as receitas

--- src/Backend/MeuLivroDeReceitas.Application/UseCases/Dashboard/DashboardUseCase.cs
+++ src/Backend/MeuLivroDeReceitas.Application/UseCases/Dashboard/DashboardUseCase.cs
@@ -3,21 +3,28 @@ using MeuLivroDeReceitas.Application.Servicos.UsuarioLogado;
 using MeuLivroDeReceitas.Comunicacao.Requisicoes;
 using MeuLivroDeReceitas.Comunicacao.Respostas;
 using MeuLivroDeReceitas.Domain.Extension;
+using MeuLivroDeReceitas.Domain.Repositorios.Conexao;
 using MeuLivroDeReceitas.Domain.Repositorios.Receita;
-using System.Globalization;
+using System.Collections;
 
 namespace MeuLivroDeReceitas.Application.UseCases.Dashboard;
 public class DashboardUseCase: IDashboardUseCase
 {
+    private readonly IConexaoReadOnlyRepositorio _conexoesRepositorio;
     private readonly IReceitaReadOnlyRepositorio _repositorio;
     private readonly IUsuarioLogado _usuarioLogado;
     private readonly IMapper _mapper;
 
-    public DashboardUseCase(IReceitaReadOnlyRepositorio repositorio, IUsuarioLogado usuarioLogado, IMapper mapper)
+    public DashboardUseCase(
+        IReceitaReadOnlyRepositorio repositorio,
+        IUsuarioLogado usuarioLogado,
+        IMapper mapper,
+        IConexaoReadOnlyRepositorio conexoesRepositorio)
     {
         _repositorio = repositorio;
         _usuarioLogado = usuarioLogado;
         _mapper = mapper;
+        _conexoesRepositorio = conexoesRepositorio;
     }
 
     public async Task<RespostaDashboardJson> Executar(RequisicaoDashboardJson requisicao)
@@ -28,10 +35,24 @@ public class DashboardUseCase: IDashboardUseCase
 
         receitas = Filtrar(requisicao, receitas);
 
+        var receitasUsuariosConectados = await ReceitasUsuariosConectados(requisicao, usuarioLogado);
+
+        receitas = receitas.Concat(receitasUsuariosConectados).ToList();
+
         return new RespostaDashboardJson
         {
             Receitas = _mapper.Map<List<RespostaReceitaDashboardJson>>(receitas)
         };
+    }
+
+    private async Task<IList<Domain.Entidades.Receita>> ReceitasUsuariosConectados(RequisicaoDashboardJson requisicao, Domain.Entidades.Usuario usuarioLogado)
+    {
+        var conexoes = await _conexoesRepositorio.RecuperarDoUsuario(usuarioLogado.Id);
+
+        var usuariosConectados = conexoes.Select(c => c.Id).ToList();
+        var receitasUsuariosConectados = await _repositorio.RecuperarTodasDosUsuarios(usuariosConectados);
+
+        return Filtrar(requisicao, receitasUsuariosConectados);
     }
 
     private static IList<Domain.Entidades.Receita> Filtrar(RequisicaoDashboardJson requisicao, IList<Domain.Entidades.Receita> receitas)
